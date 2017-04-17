5f0acb7aaa0006455ef6039f8b0252eeb235b40e
AlecksJohanssen <minh.dq1078@sinhvien.hoasen.edu.vn>
2017-04-17 11:08:47 +0700

    Add load more pagination

--- First IOS App/PhotosViewController.swift
+++ First IOS App/PhotosViewController.swift
@@ -13,41 +13,16 @@ import AFNetworking
 class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
     var photos = [NSDictionary]()
     var name = ""
+    var isMoreDataLoading = false
+    var isLoadingOnce = false
+    var loadMoreURL: URL?
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.rowHeight = 320;
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let userId = "3021318827"
-        let accessToken = "3021318827.1677ed0.e40de3cbc0be4e4cb64e102f79a61368"
-        let url = URL(string: "https://api.instagram.com/v1/users/\(userId)/media/recent/?access_token=\(accessToken)")
-        print("Test")
-        if let url = url {
-            let request = URLRequest(
-                url: url,
-                cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData,
-                timeoutInterval: 10)
-            let session = URLSession(
-                configuration: URLSessionConfiguration.default,
-                delegate: nil,
-                delegateQueue: OperationQueue.main)
-            print("Blah")
-            let task = session.dataTask(
-                with: request,
-                completionHandler: { (dataOrNil, response, error ) in
-                if let data = dataOrNil {
-                    if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-                        if let photoData = responseDictionary["data"] as? [NSDictionary] {
-                            self.photos = photoData
-                            self.tableView.reloadData()
-                        }
-                    }
-                }
-            })
-            task.resume()
-        }
+        loadData()
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
         tableView.addSubview(refreshControl)
@@ -90,7 +65,7 @@ class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewD
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        tableView.sectionHeaderHeight = 70
+        tableView.sectionHeaderHeight = 45
         return tableView.sectionHeaderHeight
     }
     
@@ -98,6 +73,8 @@ class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewD
         let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
         headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
         let profileView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 30))
+        let labelView = UILabel(frame: CGRect(x: 50, y: 10, width: 320, height: 30))
+        labelView.text = "Alecks Johanssen"
         profileView.clipsToBounds = true
         profileView.layer.cornerRadius = 15
         profileView.layer.borderColor = UIColor(white: 0.7, alpha: 0.8).cgColor
@@ -105,6 +82,7 @@ class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewD
         let url = URL(string: "https://scontent.cdninstagram.com/t51.2885-19/s150x150/17662850_1755198108129235_8707994940795256832_a.jpg")
         profileView.setImageWith(url!)
         headerView.addSubview(profileView)
+        headerView.addSubview(labelView)
         return headerView
 
     }
@@ -120,6 +98,83 @@ class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewD
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return photos.count
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if(!isMoreDataLoading) {
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            if(scrollView.contentOffset.y > scrollOffThreshold && tableView.isDragging) {
+                isMoreDataLoading = true
+                loadMoreData()
+                
+            }
+        }
+    }
+    
+    func loadMoreData() {
+        if let loadMoreURL = loadMoreURL {
+            let request = URLRequest(
+                url: loadMoreURL,
+                cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData,
+                timeoutInterval: 10)
+            let session = URLSession(
+                configuration: URLSessionConfiguration.default,
+                delegate: nil,
+                delegateQueue: OperationQueue.main)
+            print("Blah")
+            let task = session.dataTask(
+                with: request,
+                completionHandler: { (dataOrNil, response, error ) in
+                    if let data = dataOrNil {
+                        if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                            if let photoData = responseDictionary["data"] as? [NSDictionary] {
+                                self.photos = photoData
+                                self.isMoreDataLoading = false
+                                print(photoData)
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+            })
+            task.resume()
+        }
+
+    }
+    
+    func loadData() {
+        let userId = "3021318827"
+        let accessToken = "3021318827.1677ed0.e40de3cbc0be4e4cb64e102f79a61368"
+        let url = URL(string: "https://api.instagram.com/v1/users/\(userId)/media/recent/?access_token=\(accessToken)")
+        print("Test")
+        if let url = url {
+            let request = URLRequest(
+                url: url,
+                cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData,
+                timeoutInterval: 10)
+            let session = URLSession(
+                configuration: URLSessionConfiguration.default,
+                delegate: nil,
+                delegateQueue: OperationQueue.main)
+            print("Blah")
+            let task = session.dataTask(
+                with: request,
+                completionHandler: { (dataOrNil, response, error ) in
+                    if let data = dataOrNil {
+                        if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String:AnyObject] {
+                            let paginationURL = responseDictionary["pagination"]
+                            print(paginationURL?["next_url"])
+                            var pagiURL = paginationURL?["next_url"] as? String
+                            self.loadMoreURL = URL(string: pagiURL!)
+                            if let photoData = responseDictionary["data"] as? [NSDictionary] {
+                                self.photos = photoData
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+            })
+            task.resume()
+        }
     }
 
     override func didReceiveMemoryWarning() {
